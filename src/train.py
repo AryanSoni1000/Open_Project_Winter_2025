fd1a3e80330530c64e448c22f9c9c0267e7bc026
AryanSoni1000 <aryan_s@ch.iitr.ac.in>
2026-02-17 22:56:12 +0530

    Implement training function for MLQSTModel

--- src/train.py
+++ src/train.py
@@ -1,1 +1,23 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from model import MLQSTModel
+
+def train_model(X, y, epochs=100, lr=0.001):
+
+    model = MLQSTModel(X.shape[1], y.shape[1])
+    criterion = nn.MSELoss()
+    optimizer = optim.Adam(model.parameters(), lr=lr)
+
+    for epoch in range(epochs):
+        optimizer.zero_grad()
+        outputs = model(X)
+        loss = criterion(outputs, y)
+        loss.backward()
+        optimizer.step()
+
+        if epoch % 10 == 0:
+            print(f"Epoch {epoch}, Loss: {loss.item()}")
+
+    return model
 
